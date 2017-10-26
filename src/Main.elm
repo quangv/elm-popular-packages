286140f4ae1544f7a528ffece17a1a8f6ba4e720
Quang Van <quang@absentsoft.com>
2017-10-25 21:20:15 -0400

    Update

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,13 @@
 module Main exposing (..)
 
 import Html exposing (Html)
-import Http
 
 
 -- Local
 
-import Types exposing (Model, Package, PackageRow, Msg(..))
+import Types exposing (Model, Msg)
 import Model
+import Update
 import View
 
 
@@ -17,57 +17,14 @@ import View
    fetchPackages =
        Http.get ("https://cors-anywhere.herokuapp.com/" ++ "http://package.elm-lang.org/all-packages") packagesDecoder
 -}
----- UPDATE ----
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        FetchPackages ->
-            let
-                m =
-                    Debug.log "msg" msg
-            in
-                ( model, Cmd.none )
-
-        LoadPackages response ->
-            let
-                m =
-                    Debug.log "msg" (String.left 20 (toString msg))
-            in
-                ( { model
-                    | loadingPackages = False
-                    , packages = resultToList response
-                  }
-                , Cmd.none
-                )
-
-        SetTableState newState ->
-            ( { model | tableState = newState }
-            , Cmd.none
-            )
-
-
-resultToList : Result Http.Error (List Package) -> List Package
-resultToList result =
-    case result of
-        Err msg ->
-            []
-
-        Ok packages ->
-            packages
-
-
-
----- VIEW ----
 ---- PROGRAM ----
 
 
 main : Program Never Model Msg
 main =
     Html.program
-        { view = View.view
-        , init = Model.init
-        , update = update
+        { init = Model.init
+        , update = Update.update
+        , view = View.view
         , subscriptions = always Sub.none
         }
---
+++ src/Update.elm
@@ -0,0 +1,42 @@
+module Update exposing (..)
+
+import Http
+import Types exposing (Model, Package, PackageRow, Msg(..))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        FetchPackages ->
+            let
+                m =
+                    Debug.log "msg" msg
+            in
+                ( model, Cmd.none )
+
+        LoadPackages response ->
+            let
+                m =
+                    Debug.log "msg" (String.left 20 (toString msg))
+            in
+                ( { model
+                    | loadingPackages = False
+                    , packages = resultToList response
+                  }
+                , Cmd.none
+                )
+
+        SetTableState newState ->
+            ( { model | tableState = newState }
+            , Cmd.none
+            )
+
+
+resultToList : Result Http.Error (List Package) -> List Package
+resultToList result =
+    case result of
+        Err msg ->
+            []
+
+        Ok packages ->
+            packages
