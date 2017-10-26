6190bf544754d671a8ba81fc7d41fdf38f8cafa7
Quang Van <quang@absentsoft.com>
2017-10-25 21:11:43 -0400

    View

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,14 @@
 module Main exposing (..)
 
-import Array
-import Html exposing (Html, text, div, h1, button)
-import Html.Events exposing (onClick)
+import Html exposing (Html)
 import Http
-import Json.Decode as Decode exposing (decodeString, field, string, list)
-import Table
 
 
 -- Local
 
 import Types exposing (Model, Package, PackageRow, Msg(..))
 import Model
+import View
 
 
 ---- API ----
@@ -63,66 +60,13 @@ resultToList result =
 
 
 ---- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ h1 [] [ text "Elm Popular Packages" ]
-        , div []
-            [ if model.loadingPackages then
-                text "Loading..."
-              else
-                text ""
-            ]
-        , button [ onClick FetchPackages ] [ text "Fetch" ]
-        , Table.view config model.tableState (List.map mapPackages model.packages)
-
-        --, div [] (List.map viewPackage model.packages)
-        ]
-
-
-viewPackage : Package -> Html Msg
-viewPackage package =
-    div []
-        [ text package.name
-        ]
-
-
-mapPackages : Package -> PackageRow
-mapPackages pkg =
-    let
-        name =
-            Array.fromList (String.split "/" pkg.name)
-    in
-        PackageRow pkg.name pkg.summary (Maybe.withDefault "" (Array.get 0 name)) (Maybe.withDefault "" (Array.get 1 name))
-
-
-
----- TABLE CONFIGURATION ----
-
-
-config : Table.Config { a | repoName : String, username : String, name : String } Msg
-config =
-    Table.config
-        { toId = .name
-        , toMsg = SetTableState
-        , columns =
-            [ Table.stringColumn "Name" .name
-            , Table.stringColumn "Username" .username
-            , Table.stringColumn "Repo Name" .repoName
-            ]
-        }
-
-
-
 ---- PROGRAM ----
 
 
 main : Program Never Model Msg
 main =
     Html.program
-        { view = view
+        { view = View.view
         , init = Model.init
         , update = update
         , subscriptions = always Sub.none
